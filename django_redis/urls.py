ef404ac22b503a71d325020ac8ee9a240bb637e0
aabezgin <aabezgin@greenatom.ru>
2022-07-23 20:37:42 +0700

    working admin and media

--- django_redis/urls.py
+++ django_redis/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
 from user_selection.views import UserViewSet
@@ -12,4 +14,4 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
