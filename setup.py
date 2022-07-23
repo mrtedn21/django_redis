182cb5776704866fa96228c447190a4a4e852c7a
aabezgin <aabezgin@greenatom.ru>
2022-07-23 18:09:17 +0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Django_Redis',
+    version='0.1.0',
+    description='user selection app',
+    author='Bezgin Alexandr',
+    author_email='bezgin.sasha06@gmail.com',
+    install_requires=[
+        'django',
+        'djangorestframework',
+    ],
+)
+
