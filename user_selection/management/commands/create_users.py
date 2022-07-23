9dcb19e867cafb872a28f08a55c12670b93ae1c2
aabezgin <aabezgin@greenatom.ru>
2022-07-23 21:08:01 +0700

    create command

---
+++ user_selection/management/commands/create_users.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand
+
+from user_selection.models import User
+
+
+class Command(BaseCommand):
+    help = 'Create users of three types'
+
+    def handle(self, *args, **options):
+        user = User.objects.create_superuser(
+            username='admin',
+            email='admin@gmail.com',
+            password='123',
+            offer=True,
+            role_choice=User.ADMIN,
+        )
+        user.avatar.save('test.jpg', open('/app/default_images/admin.jpg', 'rb'))
+        user.save()
+
+        user = User.objects.create_user(
+            username='user',
+            email='user@gmail.com',
+            password='123',
+            offer=True,
+            role_choice=User.USERNAME_FIELD,
+        )
+        user.avatar.save('test.jpg', open('/app/default_images/user.png', 'rb'))
+        user.save()
+
+        user = User.objects.create_user(
+            username='manager',
+            email='manager@gmail.com',
+            password='123',
+            offer=True,
+            role_choice=User.MANAGER,
+        )
+        user.avatar.save('test.jpg', open('/app/default_images/manager.jpg', 'rb'))
+        user.save()
+
